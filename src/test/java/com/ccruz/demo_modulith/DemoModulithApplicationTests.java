3c7899b2fae4c91c8fe9b4b72842289d42695c42
carlos cruz <carturo99@gmail.com>
2026-05-06 22:51:58 -0500

    First commit

---
+++ src/test/java/com/ccruz/demo_modulith/DemoModulithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccruz.demo_modulith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoModulithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
